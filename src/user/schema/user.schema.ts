624e8eb6762b9d9c08e41cc5b09f47aea2768534
AkshayKumar2211 <taara0361@gmail.com>
2026-02-20 15:37:48 +0530

    first commit

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+export enum Role {
+  SUPER_ADMIN = 'SUPER_ADMIN',
+  MANAGER = 'MANAGER',
+  RECEPTIONIST = 'RECEPTIONIST',
+  WAITER = 'WAITER',
+  COOK = 'COOK',
+  USER = 'USER',
+}
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ enum: Role, default: Role.USER })
+  role: Role;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
